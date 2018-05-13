33d8ed7064974bd659888e76637ec56424b89ed8
Cory Pease <corypease@Corys-MacBook-Pro.local>
2018-05-13 00:48:23 -0600

    PsuedoCode for hangman

---
+++ console.js
@@ -0,0 +1,12 @@
+// Hangman JavaScript
+pick a random word
+put that word into an array
+player guess
+if guess is not a 1 letter string
+  player guess again
+  else if guess is in array
+    replace blank with guess
+  else push guess into not it array
+if random word has blanks
+  guess again
+  else congratulations
