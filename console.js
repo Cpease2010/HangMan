e1a0a822e40f22b625ffc36b51dda968f55006fc
Cory Pease <corypease@Corys-MacBook-Pro.local>
2018-05-13 01:25:32 -0600

    Added code: select word and push into arr.

--- console.js
+++ console.js
@@ -1,12 +1,24 @@
 // Hangman JavaScript
-pick a random word
-put that word into an array
-player guess
-if guess is not a 1 letter string
-  player guess again
-  else if guess is in array
-    replace blank with guess
-  else push guess into not it array
-if random word has blanks
-  guess again
-  else congratulations
+
+// pick a random word
+// put word into array with placeholders
+var arrOfWords = ['secret','technical','beautiful','tennis','complicated'];
+var selectedWord = [];
+var arrLength = arrOfWords.length;
+function selectWord(arr) {
+  theWord = arr[Math.floor(Math.random() * arrLength)];
+  for (var i = 0, wordLength = theWord.length; i < wordLength; i++) {
+    selectedWord[i] = " ? "
+  };
+  console.log(theWord);
+  console.log(selectedWord);
+}
+// player guess
+// if guess is not a 1 letter string
+//   player guess again
+//   else if guess is in array
+//     replace blank with guess
+//   else push guess into not it array
+// if random word has blanks
+//   guess again
+//   else congratulations
