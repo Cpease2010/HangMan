73a6de6542c4c99d8568f8a9be0ba48d0ff3f65d
Cory Pease <corypease@Corys-MacBook-Pro.local>
2018-05-13 02:41:08 -0600

    selectedWord > wordArray, guess created

--- console.js
+++ console.js
@@ -1,24 +1,36 @@
 // Hangman JavaScript
-
+function hangman() {
 // pick a random word
 // put word into array with placeholders
 var arrOfWords = ['secret','technical','beautiful','tennis','complicated'];
-var selectedWord = [];
+var wordArray = [];
 var arrLength = arrOfWords.length;
 function selectWord(arr) {
   theWord = arr[Math.floor(Math.random() * arrLength)];
   for (var i = 0, wordLength = theWord.length; i < wordLength; i++) {
-    selectedWord[i] = " ? "
+    wordArray[i] = " ? "
   };
   console.log(theWord);
-  console.log(selectedWord);
+  console.log(wordArray);
 }
+selectWord(arrOfWords)
 // player guess
-// if guess is not a 1 letter string
-//   player guess again
-//   else if guess is in array
-//     replace blank with guess
-//   else push guess into not it array
+var guess = prompt("Please pick a letter");
+var arrOfMisses = [];
+var lettersGuessed = [];
+lettersGuessed += guess//   push guess into array
+if (guess.length !== 1) {// if guess is not a 1 letter string
+  alert("You can only pick 'ONE LETTER'")//   player alerted
+} else {
+  for (var i = 0; i < theWord.length; i++) {
+    if (guess === theWord[i]) {//   else if guess is in array
+      wordArray[i] = guess//     replace blank with guess
+    }
+  }
+}
+alert(wordArray);
+alert(lettersGuessed);
 // if random word has blanks
 //   guess again
 //   else congratulations
+}
